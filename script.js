b324392ca70b13354dd8a2046115e52b43439f8e
supersakana <zacwilliamson@icloud.com>
2022-01-03 14:07:03 -0600

    Added the hover eeffect and staged button for next function

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+// VARIABLES
+let sketchGrid = document.getElementById('sketch-grid');
+let gridItem = document.createElement('div')
+let resetBtn = document.getElementById('reset-btn')
 
+
+// ADDS DIVS TO OUR 16 X 16 GRID & HOVER EVENT
 for(i = 0; i < 256; i++){
-    let gridItem = document.createElement('div');
+    let gridItem = document.createElement('div')
+    gridItem.addEventListener('mouseover', () => {
+        gridItem.style.backgroundColor = 'black';
+    })
     gridItem.className = "grid-item";
-
-    document.getElementById("sketch-grid").appendChild(gridItem);
-
-    
+    sketchGrid.appendChild(gridItem);
 }
+
+// RESETS THE COLOR ON THE SKETCH GRID
+resetBtn.addEventListener('click', () => {
+    console.log('test')
+})
+
