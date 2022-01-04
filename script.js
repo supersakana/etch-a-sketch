79cccbcacf3fbdceb72012b775981a08c76bc95c
supersakana <zacwilliamson@icloud.com>
2022-01-04 10:15:29 -0600

    added hover class to event and created clear button

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 // VARIABLES
 let sketchGrid = document.getElementById('sketch-grid');
-let gridItem = document.createElement('div')
 let resetBtn = document.getElementById('reset-btn')
 
+let gridSize = 16 * 16;
 
-// ADDS DIVS TO OUR 16 X 16 GRID & HOVER EVENT
-for(i = 0; i < 256; i++){
+
+for(i = 0; i < gridSize; i++){
     let gridItem = document.createElement('div')
     gridItem.addEventListener('mouseover', () => {
-        gridItem.style.backgroundColor = 'black';
+        gridItem.className = 'cell-hover';
     })
     gridItem.className = "grid-item";
     sketchGrid.appendChild(gridItem);
+
+
+// BUTTON THAT CLEARS THE SKETCH GRID
+    resetBtn.addEventListener('click', () => {
+        gridItem.classList.remove('cell-hover');
+    })
 }
 
-// RESETS THE COLOR ON THE SKETCH GRID
-resetBtn.addEventListener('click', () => {
-    console.log('test')
-})
+
 
