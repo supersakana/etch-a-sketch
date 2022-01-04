60d8963f783506581c1717fce6dbfb1faec72064
supersakana <zacwilliamson@icloud.com>
2022-01-04 10:45:16 -0600

    Set up a prompt so user can input width/height of sketch grid, to be continued

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // VARIABLES
 let sketchGrid = document.getElementById('sketch-grid');
 let resetBtn = document.getElementById('reset-btn')
-
+// Depends on # of grid-template-columns/rows
 let gridSize = 16 * 16;
+
 
 
 for(i = 0; i < gridSize; i++){
@@ -16,9 +17,13 @@ for(i = 0; i < gridSize; i++){
 
 // BUTTON THAT CLEARS THE SKETCH GRID
     resetBtn.addEventListener('click', () => {
+    
         gridItem.classList.remove('cell-hover');
+
+        // let userNum = prompt('Choose a grid size between 2 - 100 blocks width/height')
+
+        // if(userNum == 'test'){
+        //     console.log('I work!')
+        // }
     })
 }
-
-
-
