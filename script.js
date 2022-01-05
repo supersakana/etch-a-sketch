9b792b5657d365f1d1df3f818433e7543f3cb96d
supersakana <zacwilliamson@icloud.com>
2022-01-05 13:21:37 -0600

    Refactored the clear/prompt button

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 // VARIABLES
 let sketchGrid = document.getElementById('sketch-grid');
-let resetBtn = document.getElementById('reset-btn')
-// Depends on # of grid-template-columns/rows
+let resetBtn = document.getElementById('reset-btn');
 let gridSize = 16 * 16;
 
 
 
+
+
+
 for(i = 0; i < gridSize; i++){
-    let gridItem = document.createElement('div')
+    let gridItem = document.createElement('div');
     gridItem.addEventListener('mouseover', () => {
         gridItem.className = 'cell-hover';
     })
     gridItem.className = "grid-item";
     sketchGrid.appendChild(gridItem);
 
-
-// BUTTON THAT CLEARS THE SKETCH GRID
-    resetBtn.addEventListener('click', () => {
-    
-        gridItem.classList.remove('cell-hover');
-
-        // let userNum = prompt('Choose a grid size between 2 - 100 blocks width/height')
-
-        // if(userNum == 'test'){
-        //     console.log('I work!')
-        // }
-    })
 }
+
+// BUTTON THAT CLEARS THE SKETCH GRID AND CREATES PROMPT
+resetBtn.addEventListener('click', () => {
+    let userNum =prompt('Plug in a number to specify the width/height of the cells', '1-100');
+    if(userNum == 'test'){
+        console.log('Winner!')
+    }else{
+        alert('That was not a valid input, try again')
+    }
+
+
+    sketchGrid.querySelectorAll('.cell-hover').forEach(cell => 
+        cell.classList.remove('cell-hover'));
+
+})
